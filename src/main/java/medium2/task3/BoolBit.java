c2ce2203062f3affad49fd09a7ad34d474058308
zapjok <zoltan.apjok@mindflytech.com>
2021-12-27 14:04:03 +0100

    Java 8 Compab: Map.ofEntries changed to Stream.of .collectand Collection

--- src/main/java/medium2/task3/BoolBit.java
+++ src/main/java/medium2/task3/BoolBit.java
@@ -24,39 +24,41 @@ package medium2.task3;
 
 import java.util.AbstractMap;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class BoolBit {
     public byte[] toBitString(String word) {
         byte[] bitString = new byte[word.length()];
         char[] lettersOfWord = word.toCharArray();
-        Map<Character, Integer> indexesOfAlphabetLetters = Map.ofEntries(
-                new AbstractMap.SimpleEntry<Character, Integer>('a', 1),
-                new AbstractMap.SimpleEntry<Character, Integer>('b', 2),
-                new AbstractMap.SimpleEntry<Character, Integer>('c', 3),
-                new AbstractMap.SimpleEntry<Character, Integer>('d', 4),
-                new AbstractMap.SimpleEntry<Character, Integer>('e', 5),
-                new AbstractMap.SimpleEntry<Character, Integer>('f', 6),
-                new AbstractMap.SimpleEntry<Character, Integer>('g', 7),
-                new AbstractMap.SimpleEntry<Character, Integer>('h', 8),
-                new AbstractMap.SimpleEntry<Character, Integer>('i', 9),
-                new AbstractMap.SimpleEntry<Character, Integer>('j', 10),
-                new AbstractMap.SimpleEntry<Character, Integer>('k', 11),
-                new AbstractMap.SimpleEntry<Character, Integer>('l', 12),
-                new AbstractMap.SimpleEntry<Character, Integer>('m', 13),
-                new AbstractMap.SimpleEntry<Character, Integer>('n', 14),
-                new AbstractMap.SimpleEntry<Character, Integer>('o', 15),
-                new AbstractMap.SimpleEntry<Character, Integer>('p', 16),
-                new AbstractMap.SimpleEntry<Character, Integer>('q', 17),
-                new AbstractMap.SimpleEntry<Character, Integer>('r', 18),
-                new AbstractMap.SimpleEntry<Character, Integer>('s', 19),
-                new AbstractMap.SimpleEntry<Character, Integer>('t', 20),
-                new AbstractMap.SimpleEntry<Character, Integer>('u', 21),
-                new AbstractMap.SimpleEntry<Character, Integer>('v', 22),
-                new AbstractMap.SimpleEntry<Character, Integer>('w', 23),
-                new AbstractMap.SimpleEntry<Character, Integer>('x', 24),
-                new AbstractMap.SimpleEntry<Character, Integer>('y', 25),
-                new AbstractMap.SimpleEntry<Character, Integer>('z', 26)
-        );
+        Map<Character, Integer> indexesOfAlphabetLetters = Stream.of(new Object[][] {
+                { 'a', 1 },
+                { 'b', 2 },
+                { 'c', 3 },
+                { 'd', 4 },
+                { 'e', 5 },
+                { 'f', 6 },
+                { 'g', 7 },
+                { 'h', 8 },
+                { 'i', 9 },
+                { 'j', 10 },
+                { 'k', 11 },
+                { 'l', 12 },
+                { 'm', 13 },
+                { 'n', 14 },
+                { 'o', 15 },
+                { 'p', 16 },
+                { 'q', 17 },
+                { 'r', 18 },
+                { 's', 19 },
+                { 't', 20 },
+                { 'u', 21 },
+                { 'v', 22 },
+                { 'w', 23 },
+                { 'x', 24 },
+                { 'y', 25 },
+                { 'z', 26 }
+        }).collect(Collectors.toMap(data -> (Character) data[0], data -> (Integer) data[1]));
         for ( int i = 0; i < word.length(); i++) {
             if ( indexesOfAlphabetLetters.get(lettersOfWord[i]) % 2 == 0) {
                 bitString[i] = 0;
--- src/main/java/task15/NameGame.java
+++ src/main/java/task15/NameGame.java
@@ -20,43 +20,45 @@ package task15;
 
 import java.util.AbstractMap;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class NameGame {
     public String nameScore (String input) {
         int score = 0;
-        Map<String, Integer> scoreTable = Map.ofEntries(
-                new AbstractMap.SimpleEntry<String, Integer>("A", 100),
-                new AbstractMap.SimpleEntry<String, Integer>("B", 14),
-                new AbstractMap.SimpleEntry<String, Integer>("C", 9),
-                new AbstractMap.SimpleEntry<String, Integer>("D", 28),
-                new AbstractMap.SimpleEntry<String, Integer>("E", 145),
-                new AbstractMap.SimpleEntry<String, Integer>("F", 12),
-                new AbstractMap.SimpleEntry<String, Integer>("G", 3),
-                new AbstractMap.SimpleEntry<String, Integer>("H", 10),
-                new AbstractMap.SimpleEntry<String, Integer>("I", 200),
-                new AbstractMap.SimpleEntry<String, Integer>("J", 100),
-                new AbstractMap.SimpleEntry<String, Integer>("K", 114),
-                new AbstractMap.SimpleEntry<String, Integer>("L", 100),
-                new AbstractMap.SimpleEntry<String, Integer>("M", 25),
-                new AbstractMap.SimpleEntry<String, Integer>("N", 450),
-                new AbstractMap.SimpleEntry<String, Integer>("O", 80),
-                new AbstractMap.SimpleEntry<String, Integer>("P", 2),
-                new AbstractMap.SimpleEntry<String, Integer>("Q", 12),
-                new AbstractMap.SimpleEntry<String, Integer>("R", 400),
-                new AbstractMap.SimpleEntry<String, Integer>("S", 113),
-                new AbstractMap.SimpleEntry<String, Integer>("T", 405),
-                new AbstractMap.SimpleEntry<String, Integer>("U", 11),
-                new AbstractMap.SimpleEntry<String, Integer>("V", 10),
-                new AbstractMap.SimpleEntry<String, Integer>("W", 10),
-                new AbstractMap.SimpleEntry<String, Integer>("X", 3),
-                new AbstractMap.SimpleEntry<String, Integer>("Y", 210),
-                new AbstractMap.SimpleEntry<String, Integer>("Z", 23)
-        );
+        Map<Character, Integer> scoreTable = Stream.of(new Object[][] {
+                { 'A', 100},
+                { 'B', 14 },
+                { 'C', 9 },
+                { 'D', 28 },
+                { 'E', 145 },
+                { 'F', 12 },
+                { 'G', 3 },
+                { 'H', 10 },
+                { 'I', 200},
+                { 'J', 100 },
+                { 'K', 114 },
+                { 'L', 100 },
+                { 'M', 25 },
+                { 'N', 450 },
+                { 'O', 80 },
+                { 'P', 2 },
+                { 'Q', 12 },
+                { 'R', 400 },
+                { 'S', 113 },
+                { 'T', 405 },
+                { 'U', 11 },
+                { 'V', 10 },
+                { 'W', 10 },
+                { 'X', 3 },
+                { 'Y', 210 },
+                { 'Z', 23 }
+        }).collect(Collectors.toMap(data -> (Character) data[0], data -> (Integer) data[1]));
         char[] inputChars = input.toCharArray();
         for (char actual : inputChars) {
             // Can be confusing
             // Tha naming convention or the algorithm, choosen objects?
-            score += scoreTable.get(actual+"");
+            score += scoreTable.get(actual);
         }
         if (score <= 60) {
             return "NOT TOO GOOD";
